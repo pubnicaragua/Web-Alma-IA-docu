6c4bbdc224ce299d32b9539b044c729dfaa2320d
OfilioHA <herdocia20@gmail.com>
2025-09-25 15:24:16 -0600

    feat: :sparkles: New links and QR for apps

--- app/(auth)/download/page.tsx
+++ app/(auth)/download/page.tsx
@@ -126,7 +126,6 @@ export default function AndroidDownload() {
             <div className="flex flex-wrap justify-center gap-4">
               {/* Botón de descarga Android */}
               <motion.a
-                href="https://almaia.cl/app-android-almaia.apk"
                 onClick={() => setShowQRAndroidModal(true)}
                 download={`${APP_INFO.name}.apk`}
                 className="flex items-center gap-2 px-6 py-3 text-white rounded-full font-semibold bg-green-500 transition-all shadow-lg hover:bg-green-600"
@@ -134,7 +133,9 @@ export default function AndroidDownload() {
                 whileTap={{ scale: 0.97 }}
                 aria-label={`Descargar ${APP_INFO.name} para Android`}
               >
-                <ArrowDownTrayIcon className="w-5 h-5" />
+                <svg width={"32"} height={"32"} fill="currentColor" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M23.35 12.653l2.496-4.323c0.044-0.074 0.070-0.164 0.070-0.26 0-0.287-0.232-0.519-0.519-0.519-0.191 0-0.358 0.103-0.448 0.257l-0.001 0.002-2.527 4.377c-1.887-0.867-4.094-1.373-6.419-1.373s-4.532 0.506-6.517 1.413l0.098-0.040-2.527-4.378c-0.091-0.156-0.259-0.26-0.45-0.26-0.287 0-0.519 0.232-0.519 0.519 0 0.096 0.026 0.185 0.071 0.262l-0.001-0.002 2.496 4.323c-4.286 2.367-7.236 6.697-7.643 11.744l-0.003 0.052h29.991c-0.41-5.099-3.36-9.429-7.57-11.758l-0.076-0.038zM9.098 20.176c-0 0-0 0-0 0-0.69 0-1.249-0.559-1.249-1.249s0.559-1.249 1.249-1.249c0.69 0 1.249 0.559 1.249 1.249v0c-0.001 0.689-0.559 1.248-1.249 1.249h-0zM22.902 20.176c-0 0-0 0-0 0-0.69 0-1.249-0.559-1.249-1.249s0.559-1.249 1.249-1.249c0.69 0 1.249 0.559 1.249 1.249v0c-0.001 0.689-0.559 1.248-1.249 1.249h-0z" />
+                </svg>
                 Descargar Android
               </motion.a>
 
@@ -147,7 +148,9 @@ export default function AndroidDownload() {
                 whileTap={{ scale: 0.97 }}
                 aria-label={`Descargar ${APP_INFO.name} para iOS`}
               >
-                <ArrowDownTrayIcon className="w-5 h-5" />
+                <svg width={"32"} height={"32"} fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M18.71 19.5C17.88 20.74 17 21.95 15.66 21.97C14.32 22 13.89 21.18 12.37 21.18C10.84 21.18 10.37 21.95 9.09997 22C7.78997 22.05 6.79997 20.68 5.95997 19.47C4.24997 17 2.93997 12.45 4.69997 9.39C5.56997 7.87 7.12997 6.91 8.81997 6.88C10.1 6.86 11.32 7.75 12.11 7.75C12.89 7.75 14.37 6.68 15.92 6.84C16.57 6.87 18.39 7.1 19.56 8.82C19.47 8.88 17.39 10.1 17.41 12.63C17.44 15.65 20.06 16.66 20.09 16.67C20.06 16.74 19.67 18.11 18.71 19.5ZM13 3.5C13.73 2.67 14.94 2.04 15.94 2C16.07 3.17 15.6 4.35 14.9 5.19C14.21 6.04 13.07 6.7 11.95 6.61C11.8 5.46 12.36 4.26 13 3.5Z" />
+                </svg>
                 Descargar iOS
               </motion.a>
             </div>
@@ -227,6 +230,16 @@ export default function AndroidDownload() {
             <p className="text-xs text-gray-500 text-center">
               Compatible con iOS 13 o superior
             </p>
+            <div>
+              <a
+                target="_blank"
+                href="https://expo.dev/accounts/dxgabalt/projects/almaia/builds/78ea88b5-8380-4cc5-bfad-79c75bade5e8"
+                className="flex items-center gap-2 px-6 py-3 text-white rounded-full font-semibold bg-blue-500 transition-all shadow-lg hover:bg-blue-600"
+              >
+                <ArrowDownTrayIcon className="w-5 h-5" />
+                Descargar
+              </a>
+            </div>
           </div>
         </DialogContent>
       </Dialog>
@@ -251,6 +264,16 @@ export default function AndroidDownload() {
             <p className="text-xs text-gray-500 text-center">
               Compatible con android 6.0 o superior
             </p>
+            <div>
+              <a
+                target="_blank"
+                href="https://expo.dev/accounts/dxgabalt/projects/almaia/builds/3abf1105-ea60-42bf-9698-c688ed97972b"
+                className="flex items-center gap-2 px-6 py-3 text-white rounded-full font-semibold bg-blue-500 transition-all shadow-lg hover:bg-blue-600"
+              >
+                <ArrowDownTrayIcon className="w-5 h-5" />
+                Descargar
+              </a>
+            </div>
           </div>
         </DialogContent>
       </Dialog>
