6d74f18a8951b2353b83526e4a26adbe81d475eb
pubnicaragua <clientesactivos4@gmail.com>
2025-10-01 12:38:09 -0600

    fix: :bug: Enable catcha again

--- components/ui/recaptcha.tsx
+++ components/ui/recaptcha.tsx
@@ -4,7 +4,6 @@ import { forwardRef } from "react";
 type ReCaptchaInputProps = Omit<ReCAPTCHAProps, "sitekey">;
 
 export const ReCaptchaInput = forwardRef<ReCAPTCHA, ReCaptchaInputProps>((props, ref) => {
-  console.log(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY);  
   return (
     <ReCAPTCHA
       ref={ref}
