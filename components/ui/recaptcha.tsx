d8ff7fd7fbb4e4532462db706aabcffc27e44462
pubnicaragua <clientesactivos4@gmail.com>
2025-10-01 12:25:33 -0600

    chore: :poop: Testing if env variables are set

--- components/ui/recaptcha.tsx
+++ components/ui/recaptcha.tsx
@@ -4,6 +4,7 @@ import { forwardRef } from "react";
 type ReCaptchaInputProps = Omit<ReCAPTCHAProps, "sitekey">;
 
 export const ReCaptchaInput = forwardRef<ReCAPTCHA, ReCaptchaInputProps>((props, ref) => {
+  console.log(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY); 
   return (
     <ReCAPTCHA
       ref={ref}
